ae86bb00e3d8d6ff8355fc4dce99ba7485f46511
Scott Young <sbyoung25@gmail.com>
2022-07-19 23:00:23 -0500

    header + footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Proshop</h1>
+    <div className='App'>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
