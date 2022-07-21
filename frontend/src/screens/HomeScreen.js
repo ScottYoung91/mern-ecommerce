6d455bce614c7e00c2f4b542cc53efa25234b1be
ScottYoung91 <32314440+ScottYoung91@users.noreply.github.com>
2022-07-20 21:38:49 -0500

    axios added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import products from "../products";
-import Product from "../components/Product";
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
-  return (
-    <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={6} md={4} lg={2} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </>
-  );
+	return (
+		<>
+			<h1>Latest Products</h1>
+			<Row>
+				{products.map((product) => (
+					<Col key={product._id} sm={6} md={4} lg={2} xl={3}>
+						<Product product={product} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
 };
 
 export default HomeScreen;
